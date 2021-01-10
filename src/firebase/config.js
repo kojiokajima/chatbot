b127903533d856a8c702ae0c91aa8bf660c8e91c
wanko <kouji124@w4.dion.ne.jp>
2021-01-10 02:48:54 -0800

    init commit

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1K5w452mktz1XhdAmXOGwKM7llQ_fkEA",
+  authDomain: "chatbot-demo-19abd.firebaseapp.com",
+  projectId: "chatbot-demo-19abd",
+  storageBucket: "chatbot-demo-19abd.appspot.com",
+  messagingSenderId: "849852593398",
+  appId: "1:849852593398:web:ae7ea2d7c1ee739ab728d7",
+  measurementId: "G-B9P97G2YCE"
+};
+
+export default firebaseConfig
